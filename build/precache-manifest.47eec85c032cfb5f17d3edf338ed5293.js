c0b5f9c1ecdf9d6cb12c516f039f71b9fa119d3e
Brian Loc Nguyen <34972651+NguyenBrianL97@users.noreply.github.com>
2019-10-25 04:34:09 -0700

    Website Revamp
    Need to github pages
    github repository links
    change pictures
    integrate react
    
    build project support

---
+++ build/precache-manifest.47eec85c032cfb5f17d3edf338ed5293.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816303b74e812d390b0e2f29802df194",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3acfd63bcd639b4dbc8",
+    "url": "/static/css/main.1f8036e7.chunk.css"
+  },
+  {
+    "revision": "623be66b849187803919",
+    "url": "/static/js/2.4d10d150.chunk.js"
+  },
+  {
+    "revision": "a3acfd63bcd639b4dbc8",
+    "url": "/static/js/main.f0124d23.chunk.js"
+  },
+  {
+    "revision": "5bfcb64c3b7ccfa9e599",
+    "url": "/static/js/runtime-main.02dd13ee.js"
+  }
+]);
